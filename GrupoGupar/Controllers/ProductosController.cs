9bc3e33b841988e9b7b892729cc1c9f923ee21af
Roberto <betomax_16@live.com.mx>
2020-11-06 12:03:55 -0600

    Metodo privado para queries sql

--- GrupoGupar/Controllers/ProductosController.cs
+++ GrupoGupar/Controllers/ProductosController.cs
@@ -47,6 +47,31 @@ namespace GrupoGupar.Controllers
             }
         }
 
+        private void OldQuery()
+        {
+            //StringBuilder query = new StringBuilder("select [Productos].[id] ,[Productos].[nombre] ,[descripcion] ,[idCategoria] ,[Categorias].[nombre] as Categoria from productos inner join Categorias on Productos.idCategoria = Categorias.id where ");
+            //foreach (PropertyInfo propertyInfo in producto.GetType().GetProperties())
+            //{
+            //    var data = propertyInfo.GetValue(producto, null);
+            //    string value = data == null ? null : data.ToString();
+            //    if (!string.IsNullOrEmpty(value))
+            //    {
+            //        query.Append(string.Concat("Productos.", propertyInfo.Name, " like '", $"%{value}%' and "));
+            //    }
+            //}
+
+            //if (query.ToString().Substring(query.Length - 6, 5) == "where")
+            //{
+            //    query = query.Remove(query.Length - 6, 5);
+            //}
+            //else
+            //{
+            //    query = query.Remove(query.Length - 5, 4);
+            //}
+
+            //this.main.dgProductos.ItemsSource = db.Productos.SqlQuery(query.ToString()).ToList();
+        }
+
         public void SearchProducts()
         {
 
@@ -61,29 +86,8 @@ namespace GrupoGupar.Controllers
                     producto.idCategoria = this.main.cbCategoria.SelectedValue.ToString();
                 }
 
-                //StringBuilder query = new StringBuilder("select [Productos].[id] ,[Productos].[nombre] ,[descripcion] ,[idCategoria] ,[Categorias].[nombre] as Categoria from productos inner join Categorias on Productos.idCategoria = Categorias.id where ");
-                //foreach (PropertyInfo propertyInfo in producto.GetType().GetProperties())
-                //{
-                //    var data = propertyInfo.GetValue(producto, null);
-                //    string value = data == null ? null : data.ToString();
-                //    if (!string.IsNullOrEmpty(value))
-                //    {
-                //        query.Append(string.Concat("Productos.", propertyInfo.Name, " like '", $"%{value}%' and "));
-                //    }
-                //}
-
-                //if (query.ToString().Substring(query.Length - 6, 5) == "where")
-                //{
-                //    query = query.Remove(query.Length - 6, 5);
-                //}
-                //else
-                //{
-                //    query = query.Remove(query.Length - 5, 4);
-                //}
-
-                //this.main.dgProductos.ItemsSource = db.Productos.SqlQuery(query.ToString()).ToList();
                 var query = from p in db.Productos
-                            join c in db.Categorias on p.idCategoria equals c.id
+                            join c in db.Categorias on p.idCategoria equals c.id 
                             select new ProductoCategoria
                             {
                                 id = p.id,
